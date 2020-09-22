e241b1c48cd7b455104152d779a56342fec01f9a
pqthinh <phamquangquang2008@gmail.com>
2020-09-22 10:58:41 +0700

    push

--- lab03_FormNhap/js/index.js
+++ lab03_FormNhap/js/index.js
@@ -1,8 +1,8 @@
 document.getElementById('name').focus()
-
+document.getElementById('name').onkeyup = 
 function doKeyUp(e, current, next) {
     if (window.event) e = window.event;
-    
+
     if(e.keyCode == 13) {
         document.getElementById(next).focus();
     }
