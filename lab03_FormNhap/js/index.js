ffb0d0d1a4e506cf79826f6199ef0bfda89c0739
pqthinh <phamquangquang2008@gmail.com>
2020-09-25 00:45:09 +0700

    onkeyup event js

--- lab03_FormNhap/js/index.js
+++ lab03_FormNhap/js/index.js
@@ -1,9 +1,44 @@
 document.getElementById('name').focus()
-document.getElementById('name').onkeyup = 
+
+document.getElementById('name').onkeyup = function(e) {
+    doKeyUp(e, this, "place")
+}
+document.getElementById("place").onkeyup = function(e) {
+    doKeyUp(e, this, "birth")
+}
+document.getElementById("birth").onkeyup = function(e) {
+    doKeyUp(e, this, "email")
+}
+document.getElementById("email").onkeyup = function(e) {
+    doKeyUp(e, this, "phone")
+}
+document.getElementById('phone').onkeyup = function(e) {
+    doKeyUp(e, this, "username")
+}
+document.getElementById('username').onkeyup = function(e) {
+    doKeyUp(e, this, "pass")
+}
+document.getElementById('pass').onkeyup = function(e) {
+    doKeyUp(e, this, "repass")
+}
+document.getElementById('repass').onkeyup = function(e) {
+    doKeyUp(e, this, "note")
+}
 function doKeyUp(e, current, next) {
+    console.log(e)
     if (window.event) e = window.event;
 
     if(e.keyCode == 13) {
         document.getElementById(next).focus();
     }
 }
+
+
+
+
+// function submit() {
+//     //return
+// }
+// function quit() {
+//     //return
+// }
